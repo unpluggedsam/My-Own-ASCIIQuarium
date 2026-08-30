11b738ae8d072b35d749ddbc84fc901cb6a73c33
Sam <unpluggedsam990@gmail.com>
2026-08-30 14:52:23 -0700

    Move text_chunk functions to fish.c

---
+++ asciiquarium/src/animation_object_types/fish.c
@@ -0,0 +1,49 @@
+char *small_fish_one(void)
+{
+    return "><(((o>";
+}
+
+char *small_fish_two(void)
+{
+    return "><((o))>";
+}
+
+char *small_fish_three(void)
+{
+    return "><((o)))>";
+}
+
+char *large_fish_one(void)
+{
+    return "      __\n"
+           "  ___/  \\__\n"
+           " /         _\\\n"
+           "<          _)\n"
+           " \\________/\n";
+}
+
+char *large_fish_two(void)
+{
+    return "       ___\n"
+           "  ____/   \\___\n"
+           " /           _\\\n"
+           "<           _)\n"
+           " \\_________/\n";
+}
+
+char *large_fish_three(void)
+{
+    return "        ___\n"
+           "   ____/   \\____\n"
+           "  /             _\\\n"
+           " <       o      _)\n"
+           "  \\____________/\n";
+}
+
+char *(*text_chunk_functions[TEXT_CHUNK_COUNT])(void) = {
+    [SMALL_FISH_ONE] = small_fish_one,
+    [SMALL_FISH_TWO] = small_fish_two,
+    [SMALL_FISH_THREE] = small_fish_three,
+    [LARGE_FISH_ONE] = large_fish_one,
+    [LARGE_FISH_TWO] = large_fish_two,
+    [LARGE_FISH_THREE] = large_fish_three};
